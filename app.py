2532e357abd3cb42c609b108fa43bcc9bf78b521
susarac <suleyman1986@gmail.com>
2025-06-21 21:31:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -54,4 +54,5 @@ def mark_learned():
     return jsonify({"status": "ok"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
